f9154aebf126ce01d343e97c64cd29859e4213ec
iberis2 <spark@blast-team.com>
2024-07-17 21:04:12 +0900

    feat: moviesService CRUD

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,12 +9,15 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
   @Get()
-  getAll(): string {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('search')
@@ -22,25 +25,24 @@ export class MoviesController {
     return `We are searching for a movie after: ${searchingYear}`;
   }
 
-  @Get('/:id')
-  getOne(@Param('id') id: string): string {
-    return `This will return one movie with the id : ${id}`;
+  @Get(':id')
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
   }
 
   @Post()
   create(@Body() movieData) {
-    console.log('movieData : ', movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This will delete a movie ${id}`;
+    return this.moviesService.deleteOne(id);
   }
 
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
-    return { id: movieId, ...updateData };
+    return this.moviesService.update(movieId, updateData);
   }
 
   @Put(':id')
